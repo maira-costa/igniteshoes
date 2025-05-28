67d4ecc9b58c28ba440b6df92a8be8ab444bbe30
Maira Costa <mairaccst@gmail.com>
2025-05-28 17:14:27 -0300

    feat: onesignal and dev-client config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal'; // https://documentation.onesignal.com/docs/react-native-expo-sdk-setup
 
 import { Routes } from './src/routes';
 
@@ -8,6 +9,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("726404ba-444d-4c70-9ed2-f5d6cb13e8ac");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
